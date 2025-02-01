6bf02ab54b2f5984ede88a5c40942103b2e3b6dc
Piyush Satija <piyush.satija@gmail.com>
2025-02-01 18:51:50 +0530

    Updates

--- assets/star-DCtJdL1z.js
+++ assets/star-DCtJdL1z.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-CeIA9aNd.js";/**
+import{c as a}from"./index-DYVpmHhZ.js";/**
  * @license lucide-react v0.474.0 - ISC
  *
  * This source code is licensed under the ISC license.
