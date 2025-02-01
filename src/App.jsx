d55dd6e82a36082a64b0f730317187421fd76920
Piyush Satija <piyush.satija@gmail.com>
2025-02-01 17:59:47 +0530

    feat: add redux-persist

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,34 @@
 import { ChakraProvider, defaultSystem, Flex } from "@chakra-ui/react";
 import { Provider } from "react-redux";
 import { Navigate, Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
 
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import ConversationPage from "./pages/ConversationPage";
 import HomePage from "./pages/HomePage";
-import store from "./redux/store";
+import store, { persistor } from "./redux/store";
 
 function App() {
   return (
     <Provider store={store}>
-      <ChakraProvider value={defaultSystem}>
-        <Router>
-          <Sidebar />
-          <Flex direction="column" height="100vh" width="100%" p={2} bg="gray.50">
-            <Header />
-            <Flex flexGrow={1} overflow="hidden">
-              <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/conversation" element={<Navigate to="/" />} />
-                <Route path="/conversation/:conversationId" element={<ConversationPage />} />
-              </Routes>
+      <PersistGate loading={null} persistor={persistor}>
+        <ChakraProvider value={defaultSystem}>
+          <Router>
+            <Sidebar />
+            <Flex direction="column" height="100vh" width="100%" p={2} bg="gray.50">
+              <Header />
+              <Flex flexGrow={1} overflow="hidden">
+                <Routes>
+                  <Route path="/" element={<HomePage />} />
+                  <Route path="/conversation" element={<Navigate to="/" />} />
+                  <Route path="/conversation/:conversationId" element={<ConversationPage />} />
+                </Routes>
+              </Flex>
             </Flex>
-          </Flex>
-        </Router>
-      </ChakraProvider>
+          </Router>
+        </ChakraProvider>
+      </PersistGate>
     </Provider>
   );
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,25 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // Defaults to localStorage for web
 import conversationsReducer from "./slices/conversationsSlice";
 
-const store = configureStore({
-  reducer: {
-    conversations: conversationsReducer,
-  },
+// Create a combined reducer
+const rootReducer = combineReducers({
+  conversations: conversationsReducer,
 });
+
+// Configure Persist
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
 
 export default store;
